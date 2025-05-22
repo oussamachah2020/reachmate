5aa027ba0884fffb9a5dcecc19890ba409683d5e
oussamachah2020 <otaksam213@gmail.com>
2025-05-22 18:36:36 +0100

    fix: disable prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,19 @@
-// lib/prisma.ts
+// // lib/prisma.ts
 
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // Allow global 'prisma' to prevent multiple instances in development
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   // Allow global 'prisma' to prevent multiple instances in development
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
 
-const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"], // Optional: Log queries for debugging
-  });
+// const prisma =
+//   global.prisma ||
+//   new PrismaClient({
+//     log: ["query"], // Optional: Log queries for debugging
+//   });
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-export default prisma;
+// export default prisma;
