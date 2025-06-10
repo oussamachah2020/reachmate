1511401725608b37ed0d91881288e943aadab608
oussamachah2020 <otaksam213@gmail.com>
2025-06-10 20:22:45 +0100

    change redirection url

--- src/loaders/auth.ts
+++ src/loaders/auth.ts
@@ -40,7 +40,7 @@ async function signInWithLinkedIn() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "linkedin_oidc",
       options: {
-        redirectTo: "httpS://reachmate.xyz/confirmation",
+        redirectTo: "https://reachmate.xyz/confirmation",
       },
     });
 
